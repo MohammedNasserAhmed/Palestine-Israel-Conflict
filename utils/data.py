c82552acfdf36a61f1576763cc57792cac854744
MohammedNasserAhmed <abunasserip@gmail.com>
2023-11-10 19:49:12 +0530

    update files

--- utils/data.py
+++ utils/data.py
@@ -1,7 +1,7 @@
+from pathlib import Path
 import pandas as pd
 import os
-from IPython.core.display import display, HTML
-import matplotlib.pyplot as plt
+
 
 
 class Loader:
@@ -10,14 +10,16 @@ class Loader:
 
     Methods
     -------
-    load_csv(path): Loads and prints the data from the CSV file path if it is not None.
-    printout(data, title): Prints the data with a title.
+    read_csv(path): Loads and prints the data from the CSV file path if it is not None.
+    read_xsls(path): Loads and prints the data from the Excel file path if it is not None.
+    printout(data, show_index=False, title=None): Prints the data with a title.
     get_dataset_name(path): Returns the name of the file from the file path.
-    get_title(t, size): Displays a title using matplotlib.
+    plot_title(t, size=(12.6,0.5), fc= "#113946", pfc ="none"): Returns the title string.
     """
- 
+
     def __init__(self):
-        pass
+        with open('path/to/your/.css file', 'r') as f:
+            self.css = f.read()
 
     def read_csv(self, path):
         """
@@ -32,15 +34,25 @@ class Loader:
         if not path:
             raise ValueError("Please provide a data path.")
 
-        df = pd.read_csv(path)
+        if not Path(path).is_file():
+            raise FileNotFoundError("File does not exist at the given path.")
+
+        try:
+            df = pd.read_csv(path, encoding='utf-8')
+        except pd.errors.ParserError as e:
+            raise ValueError("Error occurred while parsing the CSV file.") from e
 
         return df
-    def read_xsls(self, path):
+
+    def read_xsls(self, path, header=None, index_col=None, usecols=None):
         """
         Load an Excel file and print its top 5 observations.
 
         Parameters:
         path (str): The path of the excel file.
+        header (int, list of int, default None): Row(s) to use as the column names.
+        index_col (int, str, sequence[int/str], or False, default None): Column(s) to set as index(MultiIndex).
+        usecols (int, str, list-like, or callable, default None): Return a subset of the columns.
 
         Returns:
         DataFrame: The loaded DataFrame.
@@ -48,13 +60,21 @@ class Loader:
         if not path:
             raise ValueError("Please provide a data path.")
 
-        df = pd.read_excel(path)
+        if not os.path.exists(path):
+            raise FileNotFoundError("File does not exist at the given path.")
+
+        if not path.endswith(('.xls', '.xlsx')):
+            raise ValueError("Invalid file extension. Only '.xls' and '.xlsx' files are supported.")
+
+        try:
+            df = pd.read_excel(path, header=header, index_col=index_col, usecols=usecols)
+        except pd.errors.ParserError as e:
+            raise ValueError("Error occurred while parsing the Excel file.") from e
 
         return df
 
-    
-
-    def printout(self, data, show_index=False, title=None):
+   
+    def dataframe_as_table(self, data, show_index=False, save_html=False, html_path=None, html_filename=None):
         """
         Display a pandas DataFrame with custom formatting.
         Removes trailing zeros and formats float numbers to 2 decimal places.
@@ -63,16 +83,34 @@ class Loader:
         data (DataFrame): The DataFrame to display.
         show_index (bool): Whether or not to show the DataFrame index.
         title (str): The title of the DataFrame.
-        """
-        with open('CSS\styles.css', 'r') as f:
-            css = f.read()
-        # Define CSS styling
+        save_html (bool): Whether or not to save the output to an HTML file.
+        html_path (str): The path to save the HTML file.
+        html_filename (str): The filename of the HTML file.
 
-        data = data.style.format("{:.2f}").set_table_styles([{"selector": "th", "props": [("background-color", "#113946"), ("color", "white"), ("font-size", "10pt")]}, {"selector": "td", "props": [("background-color", "#F9F3CC"), ("color", "black"), ("font-size", "8pt"), ("font-weight", "bold")]}])
-        styled_df = HTML(css + data.render(index=show_index))
-        if title:
-            self.plot_title(title)
-        display(styled_df)
+        Returns:
+        str: The HTML string of the formatted DataFrame.
+        """
+        if not isinstance(data, pd.DataFrame):
+            raise ValueError("The 'data' parameter must be a pandas DataFrame.")
+
+        if data.empty:
+            return None
+
+        table_styles = [{"selector": "th", "props": [("background-color", "#113946"), ("color", "white"), ("font-size", "10pt")]},
+                        {"selector": "td", "props": [("background-color", "#F9F3CC"), ("color", "black"), ("font-size", "8pt"), ("font-weight", "bold")]}]
+
+        data = data.applymap(lambda x: "{:.2f}".format(x).rstrip('0').rstrip('.') if isinstance(x, float) else x)
+        styled_df = data.style.set_table_styles(table_styles)
+
+        if save_html and Path(html_path).is_dir():
+            filename = 'styled_df.html' if html_filename is None else html_filename
+            try:
+                with open(os.path.join(html_path, filename), 'w') as f:
+                    f.write(styled_df.to_html(index=show_index))
+            except (IOError, PermissionError) as e:
+                print("Error occurred while writing to the file:", str(e))
+
+        return styled_df
 
     @staticmethod
     def get_dataset_name(path):
@@ -85,35 +123,47 @@ class Loader:
         Returns:
         str: The name of the file.
         """
-        filename = os.path.basename(path)
+        filename = Path(path).name
         name, _ = os.path.splitext(filename)
         return name
-    
-    @staticmethod
-    def plot_title(t, size=(12.6,0.5), fc= "#113946", pfc ="none"):  #fc= "#113946", pfc ="#F9F3CC"
-        """
-        Display a title using matplotlib.
 
-        Parameters:
-        t (str): The title to display.
-        size (tuple): The size of the figure. Default is (11,0.5).
-        """
-        fig, ax = plt.subplots(figsize=size)
-        ax.text(0.5, 0.5, t, fontsize=12, color=fc, ha='center', va='center',fontweight='bold')
-        ax.set_xticks([])
-        ax.set_yticks([])
-        ax.spines['top'].set_visible(False)
-        ax.spines['right'].set_visible(False)
-        ax.spines['bottom'].set_visible(False)
-        ax.spines['left'].set_visible(False)
-        ax.set_facecolor(pfc)
-        fig.patch.set_facecolor(pfc)
-        plt.show()
         
 class DataFrameCopier:
-    def __init__(self):
-        self.df = None
-    def copy_dataframe(self, df):
-        self.df = df.copy()
-    def recall_dataframe(self):
+    def __init__(self, df=None):
+        """
+        Initialize the DataFrameCopier object.
+
+        Parameters:
+        - df (pandas.DataFrame, optional): The dataframe to be copied. Defaults to None.
+        """
+        self.df = df.copy() if df is not None else None
+
+    def get_dataframe(self):
+        """
+        Get the copied dataframe.
+
+        Returns:
+        - pandas.DataFrame: The copied dataframe.
+        """
         return self.df
+
+    def __str__(self):
+        """
+        Get a string representation of the DataFrameCopier object.
+
+        Returns:
+        - str: The string representation of the DataFrameCopier object.
+        """
+        return str(self.df)
+
+    def __repr__(self):
+        """
+        Get a detailed representation of the DataFrameCopier object.
+
+        Returns:
+        - str: The detailed representation of the DataFrameCopier object.
+        """
+        return f"DataFrameCopier(df={self.df})"
+    
+loader = Loader()
+loader.get_dataset_name("E:\MyOnlineCourses\ML_Projects\palestine_israel_conflict\data\ps_il.csv")
